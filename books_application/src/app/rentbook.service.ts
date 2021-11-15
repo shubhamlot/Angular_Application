fa333ee199f363dc14be65ce99713d0cf8c172fd
Bindiya21 <bindiyakansekar@gmail.com>
2021-11-15 11:24:33 +0530

    Added methods to rentbook,returnbook and displayrentbook to rentbook services

--- books_application/src/app/rentbook.service.ts
+++ books_application/src/app/rentbook.service.ts
@@ -11,22 +11,27 @@ export class RentbookService {
   public booklist:Array<any>
   // data:Array<Books>
   selectedBooks:Books
+  //book: any;
   constructor(private http: HttpClient) {
     this.selectedBooks=new Books("","","","",0,0)
 
   }
 
- rentBook(){
+ rentBook(book:Books){
     //1.add book to user collection
     //2.remove book from local storage
- }
+   return this.http.post(this.BASE_URL+'/rentBooks'+book._id,book)
+}
 
- returnBook(){
+ returnBook(book:Books){
    //1.remove book from user collection
+   return this.http.put(this.BASE_URL+'/returnBooks/'+book._id,book)
  }
 
  displayrentedbooks(){
    //1.display book from user collection
+   return this.http.get(this.BASE_URL+'/getRentedBooks')
+  
  }
 
 }
