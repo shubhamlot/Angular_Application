3e893a23bf47a8063d0c115ce3b410149614377c
shubhamlot <shubhamlotliker99@gmail.com>
2021-11-16 10:36:54 +0530

    fixed rented books

--- books_application/src/app/app.module.ts
+++ books_application/src/app/app.module.ts
@@ -9,16 +9,13 @@ import { CreatebookComponent } from './component/createbook/createbook.component
 import { UpdatebookComponent } from './component/updatebook/updatebook.component';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { DetailsComponent } from './component/details/details.component';
-import { RentAbookComponent } from './rent-abook/rent-abook.component'
-=======
-import { DetailsComponent } from './component/details/details.component'
+import { RentAbookComponent } from './component/rent-abook/rent-abook.component'
+// import { DetailsComponent } from './component/details/details.component'
 import { SignupComponent } from './component/signup/signup.component';
 import { LoginComponent } from './component/login/login.component';
 import { CartComponent } from './component/cart/cart.component';
 import { WishlistComponent } from './component/wishlist/wishlist.component';
->>>>>>> 92ad1548597cf7616b2409b4b6168f2a2342ffc4
 
 @NgModule({
   declarations: [
@@ -27,14 +24,11 @@ import { WishlistComponent } from './component/wishlist/wishlist.component';
     CreatebookComponent,
     UpdatebookComponent,
     DetailsComponent,
-<<<<<<< HEAD
-    RentAbookComponent
-=======
+    RentAbookComponent,
 	  SignupComponent,
 	  LoginComponent,
     CartComponent,
     WishlistComponent
->>>>>>> 92ad1548597cf7616b2409b4b6168f2a2342ffc4
   ],
   imports: [
     BrowserModule,
