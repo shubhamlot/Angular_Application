382001a491ccde283eb0e4620750390dc36c0f51
suraj548 <surajgudaji548@gmail.com>
2021-11-17 16:19:16 +0530

    passport done

--- node server/routes/router.js
+++ node server/routes/router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
+const { listenerCount } = require('../model/Books')
 const book = require("../model/Books")
 const user = require("../model/Users")
 const bcrypt = require("bcrypt")
+const passport = require('passport')
 
 router.get("/getBooks",(req,res)=>{
     book.find({},(err,data)=>{
@@ -14,6 +16,11 @@ router.get("/getBooks",(req,res)=>{
     })
 })
 
+router.get("/users",(req,res)=>{
+    user.find({},(err,data)=>{
+        res.json(data)
+    })
+})
 
 router.get("/getBooks/:id",(req,res)=>{
     var _id = req.params.id
@@ -27,6 +34,17 @@ router.get("/getBooks/:id",(req,res)=>{
         }
     })
     
+})
+
+router.get("/getBooksbyCat/:category", (req, res) => {
+    book.find({ category: req.params.category }, (err, data) => {
+        if (err) {
+            res.json({ Response: "err no data found" })
+        }
+        else {
+            res.json(data)
+        }
+    })
 })
 
 router.post("/addBooks",(req,res)=>{
@@ -57,39 +75,84 @@ router.put("/editBooks/:id",async (req,res)=>{
     res.json({Response:"Status updated"})
 })
 
-router.put("/rentBooks/:id",async (req,res)=>{
-    var _id = req.params.id
+router.put("/:userid/rentBooks/:id",async (req,res)=>{
+    var bookid = req.params.id
     var copies = req.body.copies
     var rented = req.body.rented
+    var userid = req.params.userid
 
-    if(copies>0){
+    // if(copies>0){
         copies-=1
         rented+=1
-        await book.findOneAndUpdate({_id:_id},{ $set:{copies:copies,rented:rented} })
-        res.json({Response:"Status updated"})
-    }else{
-        res.json({Response:"copies are over"})
-    }
+        console.log("hi")
+        await book.findOneAndUpdate({_id:bookid},{ $set:{copies:copies,rented:rented} })
+        await user.findOneAndUpdate({_id:userid},{$push:{rentedbooks:bookid}}).then(data=>{
+            res.json({Response:"Status updated"})
+        })
+        
+       
+    // }else{
+    //     res.json({Response:"copies are over"})
+    // }
     
 })
 
 
-router.put("/returnBooks/:id",async (req,res)=>{
-    var _id = req.params.id
+router.get("/:user/rentedbooks",async (req,res)=>{
+    var userid = req.params.user
+    user.find({_id:userid},(err,data)=>{
+        if(err){
+            res.json({Response:"No data is found"})
+        }else{
+            res.json(data[0].rentedbooks)
+            // res.json(data)
+        }
+    })
+})
+
+router.put("/:user/returnBooks/:id",async (req,res)=>{
+    var bookid = req.params.id
+    var userid = req.params.user
+    
     var rented = req.body.rented
     var copies = req.body.copies
+    var list =[]
 
-    
-    if(rented<0){
-       
-        res.json({Response:"More rented then total"})
-    }else{
-      
+    user.find({_id:userid}, async (err,data)  =>{
+        flag = data[0].rentedbooks.includes(bookid)
+        
+        if(flag){
+        
         copies+=1
         rented-=1
-        await book.findOneAndUpdate({_id:_id},{ $set:{copies:copies,rented:rented} })
-        res.json({Response:"Status updated"})
-    }
+        console.log(copies)
+        for(var i=0;i<data[0].rentedbooks.length;i++){
+            if(data[0].rentedbooks[i] != bookid){
+                list.push(data[0].rentedbooks[i].toString())
+            }
+            
+        }
+      
+        
+          await book.findOneAndUpdate({_id:bookid},{ $set:{copies:copies,rented:rented} })
+          user.findOneAndUpdate({_id:userid},{$set:{rentedbooks:list}},(err,data)=>{
+             console.log(data)
+             res.json({Response:"Status updated"})
+         })
+        
+        }
+    })
+   
+    // console.log(flag)
+    // if(rented<0){
+    //     res.json({Response:"More rented then total"})
+    // }else{
+      
+    //     copies+=1
+    //     rented-=1
+    //     await book.findOneAndUpdate({_id:_id},{ $set:{copies:copies,rented:rented} })
+    //     res.json({Response:"Status updated"})
+    // }
     
 })
 
@@ -109,7 +172,7 @@ router.delete("/deleteBooks/:id",(req,res)=>{
     })
 })
 
-router.post("/signup", async (req, res) => {
+/*router.post("/signup", async (req, res) => {
 	var userObj = new user();
 	userObj.firstname = req.body.firstname;
 	userObj.lastname = req.body.lastname;
@@ -120,14 +183,26 @@ router.post("/signup", async (req, res) => {
 
 	if(duplicateUser) res.send(false);
 	else {
+        console.log(userObj.email)
 		userObj.save((err, result) => {
 			if(err) res.send(false);
-			res.send(true);
+			else res.send(true);
 		})
 	}
   //console.log(userObj)
-})
+})*/
 
+router.post(
+    '/signup',
+    passport.authenticate('signup', { session: false }),
+    async (req, res, next) => {
+      res.json({
+        message: 'Signup successful',
+        user: req.user
+      });
+    }
+  );
+  
 router.post("/login", async (req, res) => {
 	var userEmail = req.body.email;
 	var DbPassword = req.body.password;
@@ -147,4 +222,28 @@ router.post("/login", async (req, res) => {
     })
 })
 
+//returns user information
+router.get("/user-information/:email", async (req, res) => {
+    let email = req.params.email;
+
+    let result = await user.findOne({email: email});
+	
+    if(result === null) res.send(false);
+	else {
+		const userInfo = {};
+		userInfo.firstname = result.firstname;
+		userInfo.lastname = result.lastname;
+		userInfo.email = result.email;
+		userInfo.isadmin = result.isadmin;
+		userInfo.rentedbooks = result.rentedbooks;
+		res.send(userInfo);
+	}
+});
+
 module.exports = router
+
+
+
+
+
+
